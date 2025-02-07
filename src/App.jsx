afece6cf9bb84ef2361be6772d85607505bed724
Andrea Brandetti <andrebrandet93@gmail.com>
2025-02-07 19:56:16 +0100

    prova

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       
+      <h1 className='text-red-500'>Vite + React</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
